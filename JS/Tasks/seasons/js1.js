081d32edc6a58f5c41b6f1a7427b62e3ef2ea24e
omerfaruksivri <omerfaruksivri26@gmail.com>
2024-01-23 15:49:25 +0300

    seasons

--- JS/Tasks/seasons/js1.js
+++ JS/Tasks/seasons/js1.js
@@ -1,42 +1,34 @@
-let month = prompt('enter a month: ');
-switch (month) {
-case 'january':
-console.log(the season is winter);
-break;
-case 'february':
-console.log(the season is winter);
-break;
-case 'march':
-console.log(the season is spring);
-break;
-case 'april':
-console.log(the season is spring);
-break;
-case 'may':
-console.log(the season is spring);
-break;
-case 'june':
-console.log(the season is summer);
-break;
-case 'july':
-console.log(the season is summer);
-break;
-case 'august':
-console.log(the season is summer);
-break;
-case 'september':
-console.log(the season is autumn);
-break;
-case 'october':
-console.log(the season is autumn);
-break;
-case 'november':
-console.log(the season is autumn);
-break;
-case 'december':
-console.log(the season is winter);
-break;
-default:
-console.log('invalid month')
-}console.log('sum');
+let month = prompt('Bir ay girin: ');
+month = month.toLowerCase(); // Case-insensitive karşılaştırma için küçük harfe çeviriyoruz
+
+if (month === 'january' || month === 'ocak' || month === '1') {
+    result = 'Season/Mevsim: Winter/Kış';
+} else if (month === 'february' || month === 'şubat' || month === '2') {
+    result = 'Season/Mevsim: Winter/Kış';
+} else if (month === 'march' || month === 'mart' || month === '3') {
+    result = 'Season/Mevsim: Spring/Ilkbahar';
+} else if (month === 'april' || month === 'nisan' || month === '4') {
+    result = 'Season/Mevsim: Spring/Ilkbahar';
+} else if (month === 'may' || month === 'mayıs' || month === '5') {
+    result = 'Season/Mevsim: Spring/Ilkbahar';
+} else if (month === 'june' || month === 'haziran' || month === '6') {
+    result = 'Seasons/Mevsim: Summer/Yaz';
+} else if (month === 'july' || month === 'temmuz' || month === '7') {
+    result = 'Seasons/Mevsim: Summer/Yaz';
+} else if (month === 'august' || month === 'ağustos' || month === '8') {
+    result = 'Seasons/Mevsim: Summer/Yaz';
+} else if (month === 'september' || month === 'eylül' || month === '9') {
+    result = 'Seasons/Mevsim: Autumn/Sonbahar';
+} else if (month === 'october' || month === 'ekim' || month === '10') {
+    result = ('Seasons/Mevsim: Autumn/Sonbahar');
+} else if (month === 'november' || month === 'kasım' || month === '11') {
+    result = 'Seasons/Mevsim: Autumn/Sonbahar';
+} else if (month === 'december' || month === 'aralık' || month === '12') {
+    result = 'Season/Mevsim: Winter/Kış';
+} else {
+    result = 'Geçersiz ay';
 }
+
+
+
+console.log(alert(result));
