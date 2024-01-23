228fe95da8fea0f5c062bdd6d875b35a0d67554a
omerfaruksivri <omerfaruksivri26@gmail.com>
2024-01-23 15:04:38 +0300

    seasons

---
+++ JS/Tasks/seasons/js1.js
@@ -0,0 +1,42 @@
+let month = prompt('enter a month: ');
+switch (month) {
+case 'january':
+console.log(the season is winter);
+break;
+case 'february':
+console.log(the season is winter);
+break;
+case 'march':
+console.log(the season is spring);
+break;
+case 'april':
+console.log(the season is spring);
+break;
+case 'may':
+console.log(the season is spring);
+break;
+case 'june':
+console.log(the season is summer);
+break;
+case 'july':
+console.log(the season is summer);
+break;
+case 'august':
+console.log(the season is summer);
+break;
+case 'september':
+console.log(the season is autumn);
+break;
+case 'october':
+console.log(the season is autumn);
+break;
+case 'november':
+console.log(the season is autumn);
+break;
+case 'december':
+console.log(the season is winter);
+break;
+default:
+console.log('invalid month')
+}console.log('sum');
+}
