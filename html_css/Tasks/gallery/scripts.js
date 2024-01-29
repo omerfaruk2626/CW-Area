0dadd4fb6531b40f5588eaba1b88880bbc43c957
omerfaruksivri <omerfaruksivri26@gmail.com>
2024-01-29 10:54:20 +0300

    finaly

--- html_css/Tasks/gallery/scripts.js
+++ html_css/Tasks/gallery/scripts.js
@@ -26,4 +26,16 @@ function closeModal() {
     modal.style.display = 'none';  // Hide the modal
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.querySelector('.video');
+    const muteButton = document.getElementById('muteButton');
 
+    let isMuted = false;
+
+    function toggleMute() {
+        isMuted = !isMuted;
+        video.muted = isMuted;
+    }
+
+    muteButton.addEventListener('click', toggleMute);
+});
