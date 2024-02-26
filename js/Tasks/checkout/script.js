f72c4641aca5f1a67e31b0a6fdf70b04941b09a3
omerfaruk2626 <eseskikieses@gmail.com>
2024-02-26 19:25:02 +0300

    try

--- js/Tasks/checkout/script.js
+++ js/Tasks/checkout/script.js
@@ -1,33 +1,37 @@
-const plusbtn = document.querySelector(".fa-plus");
-const minusbtn = document.querySelector(".fa-minus");
-const pruductprice = document.querySelector("#product-price");
-const discount = document.querySelector("#discount");
+const plusBtn = document.querySelector(".fa-plus");
+const minusBtn = document.querySelector(".fa-minus");
+const productPrice = document.querySelector("#product-price");
 const totalprice = document.querySelector("#total-price");
-const discountedprice = document.querySelector("#discounted-price");
+const discountedPrice = document.querySelector("#discounted-price");
+const plusMinusDivs = document.querySelectorAll(".plus-minus");
+const quantity = document.querySelector(".quantity");
+const deleteBtn = document.querySelector(".fa-trash-can");
+const dollar = document.querySelector(".dollar");
 
-
-
-
-
-const plusMinusDivs = document.querySelectorAll('.plus-minus');
-
+// productPrice.textContent = parseFloat(productPrice.textContent).toFixed(2) * parseFloat(quantity.textContent).toFixed(2);
 
 plusMinusDivs.forEach((div) => {
-  const quantity = div.querySelector('.quantity');
-  const plusBtn = div.querySelector('.fa-plus');
-  const minusBtn = div.querySelector('.fa-minus');
+  const quantity = div.querySelector(".quantity");
+  const plusBtn = div.querySelector(".fa-plus");
+  const minusBtn = div.querySelector(".fa-minus");
+  const productPrice = document.querySelector("#product-price");
+  const discountedPrice = document.querySelector("#discounted-price");
 
-
-  plusBtn.addEventListener('click', () => {
+  plusBtn.addEventListener("click", () => {
     quantity.textContent = parseInt(quantity.textContent) + 1;
+    productPrice.textContent =
+      parseFloat(discountedPrice.textContent) +
+      parseFloat(productPrice.textContent);
   });
 
-  minusBtn.addEventListener('click', () => {
-    if (parseInt(quantity.textContent) > 1) {
-      quantity.textContent = parseInt(quantity.textContent) - 1;
+  minusBtn.addEventListener("click", () => {
+    if (parseInt(quantity.textContent).toFixed(2) > 1) {
+      quantity.textContent = parseInt(quantity.textContent).toFixed(2) - 1;
+      if (parseInt(quantity.textContent).toFixed(2) !== 0) {
+        productPrice.textContent =
+          parseFloat(productPrice.textContent).toFixed(2) -
+          parseFloat(discountedPrice.textContent).toFixed(2);
+      }
     }
   });
 });
-
-
-
