c71d76743b48b6f85fc8a5889a4abc3b77fa5bbb
omerfaruk2626 <eseskikieses@gmail.com>
2024-02-29 12:15:36 +0300

    final

--- js/Tasks/checkout/script.js
+++ js/Tasks/checkout/script.js
@@ -1,37 +1,120 @@
-const plusBtn = document.querySelector(".fa-plus");
-const minusBtn = document.querySelector(".fa-minus");
-const productPrice = document.querySelector("#product-price");
-const totalprice = document.querySelector("#total-price");
-const discountedPrice = document.querySelector("#discounted-price");
-const plusMinusDivs = document.querySelectorAll(".plus-minus");
-const quantity = document.querySelector(".quantity");
-const deleteBtn = document.querySelector(".fa-trash-can");
-const dollar = document.querySelector(".dollar");
+//***********************************/
+//*        CHECKOUT APP
+//***********************************/
 
-// productPrice.textContent = parseFloat(productPrice.textContent).toFixed(2) * parseFloat(quantity.textContent).toFixed(2);
+//! Donguler ile event tanımlamak mümkündür ancak fazla sayida event fazla tuketimi demektir. Bunun yerine bubbling ile parent elementler tek bir event tanımlanarak aşağıdaki elementlerin eventler yakalabilir. Yakalan event event.target ile ayrıştırılabilir.
+// function createEventsForPlusMinus() {
+//   const minusBtns = document.querySelectorAll(".fa-minus")
+//   const plusBtns = document.querySelectorAll(".fa-plus")
 
-plusMinusDivs.forEach((div) => {
-  const quantity = div.querySelector(".quantity");
-  const plusBtn = div.querySelector(".fa-plus");
-  const minusBtn = div.querySelector(".fa-minus");
-  const productPrice = document.querySelector("#product-price");
-  const discountedPrice = document.querySelector("#discounted-price");
+//   console.log(minusBtns)
+//   console.log(plusBtns)
 
-  plusBtn.addEventListener("click", () => {
-    quantity.textContent = parseInt(quantity.textContent) + 1;
-    productPrice.textContent =
-      parseFloat(discountedPrice.textContent) +
-      parseFloat(productPrice.textContent);
-  });
+//   minusBtns.forEach((minus) => {
+//     minus.addEventListener("click", () => {
+//       //? Eksilme islemleri
+//     })
+//   })
 
-  minusBtn.addEventListener("click", () => {
-    if (parseInt(quantity.textContent).toFixed(2) > 1) {
-      quantity.textContent = parseInt(quantity.textContent).toFixed(2) - 1;
-      if (parseInt(quantity.textContent).toFixed(2) !== 0) {
-        productPrice.textContent =
-          parseFloat(productPrice.textContent).toFixed(2) -
-          parseFloat(discountedPrice.textContent).toFixed(2);
-      }
+//   plusBtns.forEach((plus) => {
+//     plus.addEventListener("click", () => {
+//       //? Arttirma islemleri
+//     })
+//   })
+// }
+
+//! CONSTANTS
+
+const FREE_SHIPPING_LIMIT = 3000
+const SHIPPING_PRICE = 25.99
+const TAX_RATE = 0.18
+
+//? Selectors
+const deleteAllBtn = document.querySelector(".delete-div .fa-trash-can")
+const products = document.querySelector("article.products")
+//? EVENT HANDLERS
+
+deleteAllBtn.addEventListener("click", (e) => {
+  products.textContent = "No product"
+  products.classList.add("no-product")
+  document.querySelector(".delete-div").remove()
+  // document.querySelector(".delete-div").style.display = "none"
+  calculateTotalPrice()
+})
+
+products.addEventListener("click", (e) => {
+  // if (e.target.classList.contains("fa-plus")) {
+  //   alert("plus")
+  // } else if (e.target.classList.contains("fa-minus")) {
+  //   alert("minus")
+  // } else if (e.target.classList.contains("fa-trash-can")) {
+  //   alert("delete")
+  // }
+
+  if (e.target.classList.contains("fa-plus")) {
+    //? static olarak quantity kullanirsak hangi + butonuna tiklanilirsa tiklanilsin akistaki ilk urunun degeri artar.
+
+    //? Traversing DOM
+    e.target.previousElementSibling.textContent++
+    // document.getElementById("quantity").textContent++
+
+    // const discountedPrice = document.getElementById("discounted-price")
+    // console.log(discountedPrice.innerText)
+
+    calculatePrice(e.target)
+  } else if (e.target.classList.contains("fa-minus")) {
+    if (e.target.nextElementSibling.textContent > 1) {
+      e.target.nextElementSibling.textContent--
+      calculatePrice(e.target)
     }
-  });
-});
+  } else if (e.target.classList.contains("fa-trash-can")) {
+    e.target.closest(".product").remove()
+    calculatePrice(e.target)
+  }
+})
+
+const calculatePrice = (btn) => {
+  const discountedPrice = btn
+    .closest(".product-info")
+    .querySelector("#discounted-price")
+
+  const productPrice = btn
+    .closest(".buttons-div")
+    .querySelector("#product-price")
+
+  const quantity = btn.parentNode.querySelector("#quantity")
+
+  productPrice.textContent = (
+    quantity.textContent * discountedPrice.textContent
+  ).toFixed(2)
+
+  calculateTotalPrice()
+}
+
+const calculateTotalPrice = () => {
+  const prices = document.querySelectorAll("#product-price")
+
+  const total = [...prices].reduce(
+    (sum, price) => sum + Number(price.textContent),
+    0
+  )
+
+  const shippingPrice =
+    total >= FREE_SHIPPING_LIMIT || total === 0 ? 0.0 : SHIPPING_PRICE
+
+  const taxPrice = total * TAX_RATE
+
+  const sum = total + taxPrice + shippingPrice
+
+  //! DOM'a sonucları yazdirma
+  const selectedPrice = document.querySelector("#selected-price")
+  selectedPrice.textContent = total.toFixed(2)
+
+  document.getElementById("shipping").textContent = shippingPrice.toFixed(2)
+  document.getElementById("tax").textContent = taxPrice.toFixed(2)
+  document.getElementById("total").textContent = sum.toFixed(2)
+}
+
+window.addEventListener("load", () => {
+  calculateTotalPrice()
+})
