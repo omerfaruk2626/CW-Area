d98d60088a9e44d86a40b4512dcc4672c1f1b258
omerfaruk2626 <eseskikieses@gmail.com>
2024-03-04 18:41:52 +0300

    reset start

--- js/Tasks/idvalidator/script.js
+++ js/Tasks/idvalidator/script.js
@@ -1,27 +1,37 @@
 const btn = document.querySelector(".btn");
-const number = document.querySelector("#number");
-//? btn dinlemeye başlar
+const input = document.querySelector("#input");
+const nameInput = document.querySelector("#name")
+
 
 btn.addEventListener("click", () => {
-  let id = number.value;
-  const arrID = id.split(""); // input alanı string olduğu için split ile dizide tuttuk
-  let even = 0;
-  let odd = 0;
-  const ten = arrID.reduce((acc, cur, i) => {
-    if (i < 9) {
-      if (i % 2 !== 0) {
-        odd += Number(cur); //2nd, 4th, 6th and 8th digits
-      } else {
-        even += Number(cur); //1st, 3rd, 5th, 7th and 9th digits
-      }
-    }
-    acc = (even * 7 - odd) % 10;
-    return acc;
-  }, 0);
-  console.log(ten);
-  // let eleven =
-  if (arrID === "" || arrID[0] === 0 || arrID.length < 9) {
-    alert("Wrong ID");
-  } else {
+  event.preventDefault();
+  const idNumber = input.value;
+  const name = nameInput.value;
+
+  // const status = document.querySelector("#status");
+  // const idTable = document.querySelector("#idTable");
+  // const nameTable = document.querySelector("#nameTable");
+
+  if (idNumber == "" || name == "") {
+    alert("Please fill in all the fields.");
+    name.value = "";
+    input.value = "";
   }
+
+  const digits = idNumber.split("").map(Number);
+  const ten =
+    (digits[0] + digits[2] + digits[4] + digits[6] + digits[8]) * 7 -
+    ((digits[1] + digits[3] + digits[5] + digits[7]) % 10);
+
+  const eleven = (digits.slice(0, 9).reduce((a, b) => a + b) + ten) % 10;
+
+  if (digits[9] === ten % 10 && digits[10] === eleven) {
+    input.value = "";
+    alert("Valid ID");
+
+  } else {
+    alert("Invalid ID");
+  }
+  input.value = "";
+  name.value = "";
 });
