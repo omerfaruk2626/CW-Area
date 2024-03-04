030108c74484c6b96adb594b64a5ed2640d6febc
omerfaruk2626 <eseskikieses@gmail.com>
2024-03-04 20:51:56 +0300

    after table

--- js/Tasks/idvalidator/script.js
+++ js/Tasks/idvalidator/script.js
@@ -5,7 +5,8 @@ const nameTable = document.querySelector("#nameTable");
 const idTable = document.querySelector("#idTable");
 const statusTable = document.querySelector("#statusTable");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (event) => {
+  // console.log(input.value);
   event.preventDefault();
   const idNumber = input.value;
   const name = nameInput.value.trim();
@@ -13,25 +14,23 @@ btn.addEventListener("click", () => {
   localStorage.setItem("id", idNumber);
 
   if (name === "" || idNumber === "") {
-
     alert("Please fill in all the fields.");
   } else if (idNumber.length !== 11) {
     nameTable.innerText = name;
     idTable.innerText = idNumber;
     statusTable.innerText = "Invalid";
     statusTable.classList.add("text-danger");
-    // alert("ID number must be 11 digits long.");
   } else {
     const digits = idNumber.split("").map(Number);
     const ten =
-      (digits[0] + digits[2] + digits[4] + digits[6] + digits[8]) * 7 -
-      ((digits[1] + digits[3] + digits[5] + digits[7]) % 10);
+      ((digits[0] + digits[2] + digits[4] + digits[6] + digits[8]) * 7 -
+        (digits[1] + digits[3] + digits[5] + digits[7])) %
+      10;
     const eleven = (digits.slice(0, 9).reduce((a, b) => a + b) + ten) % 10;
-
     if (digits[9] === ten && digits[10] === eleven) {
-      alert("Valid ID");
+      statusTable.innerText = "Valid";
     } else {
-      alert("Invalid ID");
+      statusTable.innerText = "Invalid";
     }
   }
 
