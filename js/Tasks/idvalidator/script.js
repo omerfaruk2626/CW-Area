1120bcab1b025a65143bce2e4d8e59020bdd56a2
omerfaruk2626 <eseskikieses@gmail.com>
2024-03-04 23:08:01 +0300

    localden önce

--- js/Tasks/idvalidator/script.js
+++ js/Tasks/idvalidator/script.js
@@ -4,24 +4,23 @@ const nameInput = document.querySelector("#name");
 const nameTable = document.querySelector("#nameTable");
 const idTable = document.querySelector("#idTable");
 const statusTable = document.querySelector("#statusTable");
+const timeTable = document.querySelector("#timeTable");
+
+
 
 btn.addEventListener("click", (event) => {
   // console.log(input.value);
   event.preventDefault();
   const idNumber = input.value;
   const name = nameInput.value.trim();
-  // localStorage.setItem("name", name);
-  // localStorage.setItem("id", idNumber);
+  localStorage.setItem("name", name);
+  localStorage.setItem("id", idNumber);
+  
 
   if (name === "" || idNumber === "") {
     alert("Please fill in all the fields.");
   } else if (idNumber.length !== 11) {
-    nameTable.innerText = name;
-    idTable.innerText = idNumber;
-    statusTable.innerText = "Invalid";
-    statusTable.classList.add("text-danger");
-    idTable.classList.add("text-danger");
-    nameTable.classList.add("text-danger");
+    alert("ID must be 11 digits long.");
   } else {
     const digits = idNumber.split("").map(Number);
     const ten =
@@ -33,11 +32,20 @@ btn.addEventListener("click", (event) => {
       statusTable.innerText = "Valid";
       nameTable.innerText = name;
       idTable.innerText = idNumber;
-      statusTable.classList.add("text-success");
-      idTable.classList.add("text-success");
-      nameTable.classList.add("text-success");
+      statusTable.className = "text-success";
+      nameTable.className = "text-success";
+      idTable.className = "text-success";
+      timeTable.className = "text-success";
+      timeTable.innerText = timeTable.innerText = new Date().toLocaleString();
     } else {
+      nameTable.innerText = name;
+      idTable.innerText = idNumber;
       statusTable.innerText = "Invalid";
+      statusTable.classList.add("text-danger");
+      idTable.classList.add("text-danger");
+      nameTable.classList.add("text-danger");
+      timeTable.className = "text-danger";
+      timeTable.innerText = timeTable.innerText = new Date().toLocaleString();
     }
   }
 
